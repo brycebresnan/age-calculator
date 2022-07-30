36678cfe8a22dba3ce294a59c9c89642114f47f2
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-29 23:45:24 -0700

    Red: Second  test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -6,4 +6,11 @@ describe('Age', () => {
     const newAge = new Age();
     expect(typeof(newAge)).toEqual('object');
   });
+
+  test('should take in years as a parameter and set it as the value in earthYears property',() => {
+    const years = 20;
+    const newAge = new Age(years);
+    expect(newAge.earthYears).toEqual(20);
+  });
+
 });
