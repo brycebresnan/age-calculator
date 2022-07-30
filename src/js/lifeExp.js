0b6b484365aae5865aa690e3bd6f484ad879e80d
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-30 02:03:14 -0700

    create calcLifeExpect function

--- src/js/lifeExp.js
+++ src/js/lifeExp.js
@@ -2,4 +2,9 @@ export default class LifeExp {
   constructor (obj) {    
     this.age = obj.earthYears;
   }
+
+  calcLifeExpect() {
+    let result = (78.6 - this.age);
+    return parseFloat(result.toFixed(1));
+  }
 }
