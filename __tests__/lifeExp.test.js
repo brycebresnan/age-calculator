342168b1a79a0eb2cef146e74c4ed515da50351d
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-30 00:57:59 -0700

    add constructor age property test

--- __tests__/lifeExp.test.js
+++ __tests__/lifeExp.test.js
@@ -1,4 +1,5 @@
 import LifeExp from './../src/js/lifeExp.js';
+import Age from './../src/js/age.js'
 
 describe('LifeExp', () => {
 
@@ -6,4 +7,12 @@ describe('LifeExp', () => {
     const lifeEx = new LifeExp();
     expect(typeof(lifeEx)).toEqual('object');
   });
+
+  test('should take in an age object as an argument and set the this.age property value from age.earthYears', () => {
+    const years = 20;
+    const age = new Age(years);
+    const lifeEx = new LifeExp(age);
+    expect(lifeEx.age).toEqual(20);
+  });
+
 });
