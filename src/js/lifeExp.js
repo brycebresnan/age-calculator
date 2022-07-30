f77166294ff54e52302233ddb48a8f7ad52d1bd6
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-30 02:21:36 -0700

    add expect property in object

--- src/js/lifeExp.js
+++ src/js/lifeExp.js
@@ -1,11 +1,12 @@
 export default class LifeExp {
   constructor (obj) {    
     this.age = obj.earthYears;
+    this.expect = 78.6
   }
 
   calcLifeExpect() {
     const currentAge = this.age;
-    const lifeExpect = 78.6
+    const lifeExpect = 78.6;
     let result = 0;
     if (currentAge <= lifeExpect) {
       result = (lifeExpect - currentAge);
@@ -14,5 +15,6 @@ export default class LifeExp {
     }
     return parseFloat(result.toFixed(1));    
   }
-  
+
+
 }
