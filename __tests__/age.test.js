c1c0c82a5e4bbd87a6f21c1b4e6f07cd2cee447c
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-30 00:06:40 -0700

    add venus function test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -19,4 +19,9 @@ describe('Age', () => {
     expect(newAge.mercury()).toEqual(83);
   });
 
+  test('should take the Earth Years and return the equivalent in Venus years.', () => {
+    const years = 20;
+    const newAge = new Age(years);
+    expect(newAge.venus()).toEqual(32);
+  });
 });
