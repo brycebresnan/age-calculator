6e9047b2ca8ba153a192321d1685610709694e9d
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-30 01:47:30 -0700

    FIX: add object creation in first test to prevent TypeError

--- __tests__/lifeExp.test.js
+++ __tests__/lifeExp.test.js
@@ -4,14 +4,16 @@ import Age from './../src/js/age.js'
 describe('LifeExp', () => {
 
   test('should create an object type for LifeExp', () => {
-    const lifeEx = new LifeExp();
+    const years = 20;
+    const newAge = new Age(years);
+    const lifeEx = new LifeExp(newAge);
     expect(typeof(lifeEx)).toEqual('object');
   });
 
   test('should take in an age object as an argument and set the this.age property value from age.earthYears', () => {
     const years = 20;
-    const age = new Age(years);
-    const lifeEx = new LifeExp(age);
+    const newAge = new Age(years);
+    const lifeEx = new LifeExp(newAge);
     expect(lifeEx.age).toEqual(20);
   });
 
