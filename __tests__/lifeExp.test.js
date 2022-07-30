3af1d418961a50a3be7b3d579a2d345abe3e01b5
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-30 00:47:20 -0700

    FIX: add const to test syntax

--- __tests__/lifeExp.test.js
+++ __tests__/lifeExp.test.js
@@ -3,7 +3,7 @@ import lifeExp from './../src/js/lifeExp.js'
 describe('LifeExp', () => {
 
   test('should create an object type for LifeExp', () => {
-    lifeEx = new LifeExp();
+    const lifeEx = new LifeExp();
     expect(typeof(lifeEx)).toEqual('object');
   });
 });
