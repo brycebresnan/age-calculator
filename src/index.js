c1b86ef27a695791c83bc75533ad296335657a78
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-30 23:57:00 -0700

    add function to display life expectancy

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function lifeExpCalc(e) {
   let liExpArray = getExpFormData();
   let lifeExpect = new LifeExp(getAgeFormData());
   lifeExpect.expectancy(liExpArray[0],liExpArray[1]);
-  console.log(lifeExpect);
+  lifeExpDisplay(lifeExpect);
 }
 
 function getExpFormData() {
@@ -44,4 +44,22 @@ let sexCheck = document.querySelector("input[name='sex']:checked").value;
 let smokerCheck = document.querySelector("input[name='smoker']:checked").value;
 let returnArray = [sexCheck, smokerCheck];
 return returnArray;
+}
+
+function lifeExpDisplay(expObj) {
+  if (isNaN(expObj.age)) {
+    document.getElementById("longLivedCheck").innerText = "Please enter an age to calculate in the form above!";
+  } else {
+    if (expObj.age > expObj.expect) {
+      document.getElementById("longLivedCheck").innerText = "Holy cow! You beat the odds and lived past your expected life span! Here's how long you've kept yourself foot of the galactic grave:";
+    } else {
+      document.getElementById("longLivedCheck").innerText = "This is how little you've got left:";
+    }
+    document.getElementById("expectancy").innerText = " " + expObj.expect;
+    document.getElementById("earthExp").innerText = " " + expObj.lifeLeft;
+    document.getElementById("mercuryExp").innerText = " " + expObj.mercLeft;
+    document.getElementById("venusExp").innerText = " " + expObj.venusLeft;
+    document.getElementById("marsExp").innerText = " " + expObj.marsLeft;
+    document.getElementById("jupiterExp").innerText = " " + expObj.jupLeft;
+  }
 }
