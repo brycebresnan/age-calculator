69bc30eb252e326fa4fdf9f3c9b6fbbbffc1a470
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-30 22:57:27 -0700

    add submit event and gather functions for age

--- src/index.js
+++ src/index.js
@@ -4,3 +4,27 @@ import './css/styles.css';
 import Age from './js/age.js';
 import LifeExp from './js/lifeExp';
 
+window.addEventListener("load", function(){
+  const ageForm = document.getElementById('userAge');
+  ageForm.addEventListener("submit", ageCalc);
+  const expForm = document.getElementById('lifeExpect');
+  ageForm.addEventListener("submit", );
+});
+
+function getAgeFormData() {
+  let userAge = parseInt(document.getElementById("ageInput").value);
+  console.log(userAge);
+  return userAge;
+}
+
+function ageCalc(e) {
+  e.preventDefault();
+  let age = getAgeFormData();
+  let galacticAge = new Age(age);
+  console.log(galacticAge);
+  return galacticAge;
+}
+
+function getExpFormData() {
+let smokerCheck = document.getElementById("ageInput").value
+}
